0babd44087b2ec1a34731980a8841618604b4029
Yash Aryan <yash.aryan1@learner.manipal.edu>
2020-08-10 02:09:21 +0530

    Updates

---
+++ precache-manifest.fe4dbe92b1ff48b8cefc707dd192e059.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "430c52d0587ab2ff5ed09bdc72b1a55d",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "07660a72fd4e8a5b3998",
+    "url": "/pokedex/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "eb62b404e1aa09a428ad",
+    "url": "/pokedex/static/js/2.24241671.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokedex/static/js/2.24241671.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07660a72fd4e8a5b3998",
+    "url": "/pokedex/static/js/main.66d8a5ca.chunk.js"
+  },
+  {
+    "revision": "5d5b7317cf43a0b8925c",
+    "url": "/pokedex/static/js/runtime-main.2d94522c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pokedex/static/media/logo.5d5d9eef.svg"
+  }
+]);
